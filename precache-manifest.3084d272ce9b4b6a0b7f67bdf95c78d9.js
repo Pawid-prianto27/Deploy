a3ba2733f8a4321d613cb0ea5b94889bae8b566f
Pawid-prianto27 <pawitanto27@gmail.com>
2019-07-08 00:45:23 +0700

    Updates

---
+++ precache-manifest.3084d272ce9b4b6a0b7f67bdf95c78d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa85c6c2740e2b0ef7a49d616e7a98e",
+    "url": "/Pawid-prianto27/github.io.git/my-app/index.html"
+  },
+  {
+    "revision": "1353a8cf8ea5b54ce9f9",
+    "url": "/Pawid-prianto27/github.io.git/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "917e9f640dd179305a23",
+    "url": "/Pawid-prianto27/github.io.git/my-app/static/js/2.1dcc56b9.chunk.js"
+  },
+  {
+    "revision": "1353a8cf8ea5b54ce9f9",
+    "url": "/Pawid-prianto27/github.io.git/my-app/static/js/main.a1f6dc54.chunk.js"
+  },
+  {
+    "revision": "0803380238f03068f0d6",
+    "url": "/Pawid-prianto27/github.io.git/my-app/static/js/runtime~main.edc17f08.js"
+  }
+]);
