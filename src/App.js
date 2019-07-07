8f02748def3e41230ad9887f27b163e0ea383295
Pawid-prianto27 <pawitanto27@gmail.com>
2019-07-08 00:42:43 +0700

    hosting pertama

--- src/App.js
+++ src/App.js
@@ -1,26 +1,155 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Home() {
+  return <h2>Halaman Home</h2>
 }
+
+function ListView() {
+  return (
+    <div>
+    <h2>Halaman User</h2>
+    <ul>
+      <Link to='user/pawid'>Pawid</Link> <br />
+      <Link to='user/ade'>Ade</Link>
+    </ul>
+    </div>
+
+  )
+}
+function NoMatch() {
+  return <h2>404, Halaman tidak ditemukan</h2>
+}
+function DetailView({match}) {
+  return <h2>Ini halaman {match.params.name}</h2>
+}
+
+class App extends Component {
+    render() {
+      return (
+        <BrowserRouter>
+          <div>
+            <nav>
+            <li> <Link to='/'> Home </Link></li>
+
+            <li> <Link to='/users'> User </Link></li>
+            </nav>
+            <main>
+              <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/users' exact component={ListView} />
+              <Route path='/user/:name' exact component={DetailView} />
+              <Route component={NoMatch}/>
+              </Switch>
+            </main>
+          </div>
+        </BrowserRouter>
+      );
+    }
+  }
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       items: [],
+//       isLoading: true
+//     }
+//   }
+  
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then(response => response.json())
+//       .then(data => this.setState({ items: data, isLoading: false}))
+//   }
+
+  
+//   render() {
+//     const { items, isLoading } = this.state
+
+//     if(isLoading) {
+//       return <p>Loading....</p>
+//     }
+
+//     return (
+//       <div>
+//         <ul>
+//           {items.map((item, index )=> 
+//             <li key={index}>{item.name}</li>)}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// // State
+// class Timer extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       time: props.start
+//     }
+//   }
+
+//   //lifecycle
+//   componentDidMount(){
+//     this.addInterval = setInterval(() => this.increase(), 1000)
+//   }
+
+//   componenetwillUnmount(){
+//     clearInterval(this.addInterval)
+//   }
+// increase(){
+//   // update state time perdetik
+//   this.setState((state, props) => ({
+//     time: parseInt(state.time) + 1
+//   }))
+// }
+//   render() {
+//     return (
+//       <div> {this.state.time} Detik</div>
+//     )
+//   }
+// }
+
+// class Toggle extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       toggleStatus: true
+//     }
+//     this.handleClick =  this.handleClick.bind(this)
+//   }
+
+//   handleClick() {
+//     this.setState(state => ({
+//     toggleStatus: !state.toggleStatus
+//   }))
+// }
+
+//  render() {
+//    return (
+//      <button onClick={this.handleClick}>
+//       {this.state.toggleStatus ? 'ON' : 'OFF'}
+//       <p>kondisi sekarang {this.state.toggleStatus ? 'Menyala' : 'Mati'}</p>
+//      </button>
+//    )
+//  }
+// }
+// class App extends Component {
+//  render() { 
+//   return (
+//     <div className="App">
+//      <Toggle />
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//        <Timer start="0"/>      
+       
+//       </header>
+//     </div>
+//   );
+//   }
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// const name = "Pawit"
+
+
+// function greet() {
+//   return "Hallo " + name
+  
+// }
+// const elemen = <h1>{greet(name)} !!</h1>
+// ReactDOM.render( elemen, document.getElementById('master'));
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
